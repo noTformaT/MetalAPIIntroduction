ad44589c82dee34ee60ab7f70fdb7b8c4b2fb6b2
Eugene Karpenko <notformat@gmail.com>
2023-01-27 17:57:14 +0200

    Refactor HelloTriangleIOS sample

---
+++ HelloTriangleIOS/Source/AppView.swift
@@ -0,0 +1,14 @@
+//
+//  AppView.swift
+//  HelloTriangleIOS
+//
+//  Created by Eugene Karpenko on 27.01.2023.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        ContentView().frame(width: 800, height: 600)
+    }
+}
