0ab193a668fc64359e24bf765636f46c6da50540
Eugene Karpenko <notformat@gmail.com>
2023-01-30 01:38:18 +0200

    Add Gestures Sample

---
+++ Gestures/Engine/Mesh.swift
@@ -0,0 +1,12 @@
+//
+//  Mesh.swift
+//  HelloTriangleIOS
+//
+//  Created by Eugene Karpenko on 29.01.2023.
+//
+
+import Foundation
+
+class Mesh {
+    
+};
---
+++ Gestures/Source/Application.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleIOSApp.swift
+//  HelloTriangleIOS
+//
+//  Created by Eugene Karpenko on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
